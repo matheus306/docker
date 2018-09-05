dc8450c996c226b999aecb42d12be454d97fcf1d
Matheus Melo <matheus306@gmail.com>
2018-09-05 12:46:22 +0000

    validando erros na aplicação

--- NmfToWav/code/app.py
+++ NmfToWav/code/app.py
@@ -5,6 +5,7 @@ from subprocess import Popen, PIPE
 from os.path import basename
 import os
 import io
+import sys
 
 app = Flask(__name__)
 
@@ -14,27 +15,29 @@ def teste():
 
 @app.route("/v1/nmftowav/upload", methods=['POST'])
 def upload_nmf():
-	for file in request.files.getlist('files'):
-		nome_arquivo = os.path.splitext(file.filename)[0]
-		file_name = secure_filename(file.filename)
-		file_target = os.path.join('/root/', file_name)
-		file.save(file_target)
-		convert_to_wav(os.path.join('/root/', file_name))
+	try:
+		for file in request.files.getlist('files'):
+			nome_arquivo = os.path.splitext(file.filename)[0]
+			file_name = secure_filename(file.filename)
+			file_target = os.path.join('/root/', file_name)
+			file.save(file_target)
+			convert_to_wav(os.path.join('/root/', file_name))
 
+		with open('/root/' +nome_arquivo+ '.wav', mode='rb') as file:
+			fileContent = file.read()
 
-	with open('/root/' +nome_arquivo+ '.wav', mode='rb') as file:
-		fileContent = file.read()
+			response = make_response(fileContent)
+			response.headers['Content-type'] = 'audio/wav'
+			response.headers['Content-Disposition'] =  'attachment; filename=''/root/' +nome_arquivo+ '.wav'
+			response.headers['Content-Length'] = os.stat('/root/' +nome_arquivo+ '.wav').st_size
 
-		response = make_response(fileContent)
-		response.headers['Content-type'] = 'audio/wav'
-		response.headers['Content-Disposition'] =  'attachment; filename=''/root/' +nome_arquivo+ '.wav'
-		response.headers['Content-Length'] = os.stat('/root/' +nome_arquivo+ '.wav').st_size
+		os.remove('/root/' +nome_arquivo+ '.wav')
+		os.remove('/root/' +nome_arquivo+ '.nmf')
 
-	os.remove('/root/' +nome_arquivo+ '.wav')
-	os.remove('/root/' +nome_arquivo+ '.nmf')
-
-	return response
-
+		return response
+	except:
+		type, value, traceback = sys.exc_info()
+		return ('{"error" : "%s" }' % (value.strerror)), 500
 
 
 if __name__ == '__main__':
