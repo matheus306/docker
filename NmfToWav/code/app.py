f92189055160e726c4c2a2cb5dde91df426f7a94
Matheus Melo <matheus306@gmail.com>
2018-09-20 15:46:19 +0000

    Rodando o Flask no WSGI

--- NmfToWav/code/app.py
+++ NmfToWav/code/app.py
@@ -1,44 +1,44 @@
+import os
+
 from flask import Flask, request, make_response
 from werkzeug import secure_filename
+
 from NMFtoWAVConverter import convert_to_wav
-from subprocess import Popen, PIPE
-from os.path import basename
-import os
-import io
-import sys
 
 app = Flask(__name__)
 
 
-@app.route("/v1")
+@app.route("/")
 def teste():
     return "API rodando perfeitamente!"
 
+
 @app.route("/v1/nmftowav/upload", methods=['POST'])
 def upload_nmf():
-	try:
-		for file in request.files.getlist('files'):
-			nome_arquivo = os.path.splitext(file.filename)[0].replace(" ", "_")
-			file_name = secure_filename(file.filename.replace(" ", "_"))
-			file_target = os.path.join('/root/', file_name)
-			file.save(file_target)
-			convert_to_wav(os.path.join('/root/', file_name))
+    try:
+        for file in request.files.getlist('files'):
+            nome_arquivo = os.path.splitext(file.filename)[0].replace(" ", "_")
+            file_name = secure_filename(file.filename.replace(" ", "_"))
+            file_target = os.path.join('/root/', file_name)
+            file.save(file_target)
+            convert_to_wav(os.path.join('/root/', file_name))
 
-		with open('/root/' +nome_arquivo+ '.wav', mode='rb') as file:
-			fileContent = file.read()
+        with open('/root/' + nome_arquivo + '.wav', mode='rb') as file:
+            file_content = file.read()
 
-			response = make_response(fileContent)
-			response.headers['Content-type'] = 'audio/wav'
-			response.headers['Content-Disposition'] =  'attachment; filename=''/root/' +nome_arquivo+ '.wav'
-			response.headers['Content-Length'] = os.stat('/root/' +nome_arquivo+ '.wav').st_size
+            response = make_response(file_content)
+            response.headers['Content-type'] = 'audio/wav'
+            response.headers['Content-Disposition'] = 'attachment; filename=''/root/' + nome_arquivo + '.wav'
+            response.headers['Content-Length'] = os.stat('/root/' + nome_arquivo + '.wav').st_size
 
-		os.remove('/root/' +nome_arquivo+ '.wav')
-		os.remove('/root/' +nome_arquivo+ '.nmf')
+        os.remove('/root/' + nome_arquivo + '.wav')
+        os.remove('/root/' + nome_arquivo + '.nmf')
 
-		return response
-	except Exception as e:
-		return ('{"error" : "%s" }' % (e.message)), 400
+        return response
+    except Exception as e:
+        return ('{"error" : "%s" }' % e.message), 400
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host= '0.0.0.0')
+    print (">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    app.run()
---
+++ NmfToWav/code/nmf2wav.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.append('/var/www/prod')
+from app import app as application
