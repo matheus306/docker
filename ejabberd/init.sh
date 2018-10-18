dd7e008bc4a6b7b1c51a22072ba179313b3bf68f
Matheus Melo <matheus306@gmail.com>
2018-10-18 17:31:06 +0000

    init

---
+++ ejabberd/init.sh
@@ -0,0 +1,22 @@
+#! bin bash
+
+echo 'Copiando o arquivo de configuração da AeC'
+cp /home/ejabberd.yml /usr/local/etc/ejabberd/ && \
+
+echo '>>>>>>>>>>>>>>>>>>>>>>>>> Inicializando o servidor ejabberd... '
+ejabberdctl start && \
+
+echo '>>>>>>>>>>>>>>>>>>>>>>>>> Aguardando o servidor subir'
+sleep 5 && \
+
+echo '>>>>>>>>>>>>>>>>>>>>>>>>> Criando o usuario administrador...'
+ejabberdctl register admin aec 123123 && \
+
+echo '>>>>>>>>>>>>>>>>>>>>>>>>> Parando o servidor o ejabberd... '
+ejabberdctl stop && \
+
+echo '>>>>>>>>>>>>>>>>>>>>>>>>> Aguardando o servidor subir'
+sleep 5 && \
+
+echo '>>>>>>>>>>>>>>>>>>>>>>>>> Subindo o servidor ejabberd'
+ejabberdctl start
